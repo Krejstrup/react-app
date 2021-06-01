889989d8c941323879e3a23b3106e5c0bb2b1005
Richard Krejstrup <krejstrup@hotmail.com>
2021-06-01 16:30:39 +0200

    Added working submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,51 @@
 import React, { Component } from "react";
 import Table from "./Table.jsx";
+import Form from "./Form";
 
 class App extends Component {
+  // Save personal list in a state memory part (read/write)
   state = {
     characters: [
       {
-        name: "Charlie",
-        job: "Janitor",
+        Id: "13",
+        FirstName: "Charlie",
+        LastName: "Jonsson",
+        Phone: "045487654",
       },
       {
-        name: "Mac",
-        job: "Bouncer",
+        Id: "15",
+        FirstName: "Mac",
+        LastName: "Svenson",
+        Phone: "023487664",
       },
       {
-        name: "Dee",
-        job: "Aspring actress",
+        Id: "17",
+        FirstName: "Dee",
+        LastName: "Bengtson",
+        Phone: "042387684",
       },
       {
-        name: "Dennis",
-        job: "Bartender",
+        Id: "23",
+        FirstName: "Dennis",
+        LastName: "Knutson",
+        Phone: "0455687676",
       },
     ],
+  };
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        // Creating a new list instead of mutating the old
+        return i !== index;
+      }),
+    });
+  };
+
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] }); // Spread syntax '...'
   };
 
   render() {
@@ -28,23 +53,16 @@ class App extends Component {
 
     return (
       <div className="container">
+        <h2>Testar React</h2>
         <Table
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
+        <h3>Add new</h3>
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }
-
-  removeCharacter = (index) => {
-    const { characters } = this.state;
-
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index;
-      }),
-    });
-  };
 }
 
 export default App;
